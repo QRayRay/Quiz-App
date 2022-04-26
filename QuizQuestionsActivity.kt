58c9ec598110889c6294e7aad43fb6fdb97b42b9
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-04-25 20:25:48 -0500

    Update QuizQuestionsActivity.kt

--- QuizQuestionsActivity.kt
+++ QuizQuestionsActivity.kt
@@ -3,13 +3,41 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var progressBar : ProgressBar? = null
+    private var tvProgress : TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage : ImageView? = null
+
+    private var tvOptionOne:TextView? = null
+    private var tvOptionTwo:TextView? = null
+    private var tvOptionThree:TextView? = null
+    private var tvOptionFour:TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+
+
         val questionsList = Constants.getQuestions()
         Log.i("QuestionsList size is", "${questionsList.size}")
+
+        for(i in questionsList){
+            Log.e("Question", i.question) // individual question of the questions list. Class
+        }
     }
 }
