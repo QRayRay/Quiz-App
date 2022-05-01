6e47c8743bcd80f7ab3772ab3e8c752d4f287511
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-04-30 23:41:40 -0500

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+// import 10 questions on the app instead of 9.
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
